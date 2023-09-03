bcb3fbc0a9d75765225c8e6551ad5de14348ad9d
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 11:33:37 +0300

    Article props

--- Blog.Model/Models/Concrete/Article.cs
+++ Blog.Model/Models/Concrete/Article.cs
@@ -1,8 +1,29 @@
 ﻿using Blog.Model.Models.Abstract;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Model.Models.Concrete
 {
     public class Article: BaseEntity
     {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImagePath { get; set; }
+        
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+        //navigationProp
+
+        //1 makalenin 1 yazarı
+
+
+        //1 makalenin çokça beğenisi
+
+
+        //1 makalenin çokça yorumu
+
+
+        //1 makalenin 1 kategorisi
     }
 }
