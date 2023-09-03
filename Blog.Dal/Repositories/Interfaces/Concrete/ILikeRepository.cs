6030b963c7bd1054efc1b86dbd5a3c3187f07ffe
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 14:02:58 +0300

    ILikeRepository

---
+++ Blog.Dal/Repositories/Interfaces/Concrete/ILikeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Model.Models.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Dal.Repositories.Interfaces.Concrete
+{
+    public interface ILikeRepository
+    {
+        //like diğerleri gibi değil. ya oluşturursun ya silersin
+        void Create(Like like);
+        void Delete(Like  like)
+    }
+}
