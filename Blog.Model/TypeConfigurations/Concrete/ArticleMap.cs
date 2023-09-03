bc636a00108f64434473211b5679cb6a15488cae
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 12:21:10 +0300

    ArticleMap: IEntityTypeConfiguration<Article>

---
+++ Blog.Model/TypeConfigurations/Concrete/ArticleMap.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Model.Models.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Model.TypeConfigurations.Concrete
+{
+    public class ArticleMap: IEntityTypeConfiguration<Article>//Article için db configurasyonu
+    {
+    }
+}
