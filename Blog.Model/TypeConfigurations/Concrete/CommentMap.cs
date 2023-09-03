49bd0c0858eeae007d1fa125c8e89de748d12769
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 12:57:38 +0300

    public class CommentMap : BaseMap<Comment>

--- Blog.Model/TypeConfigurations/Concrete/CommentMap.cs
+++ Blog.Model/TypeConfigurations/Concrete/CommentMap.cs
@@ -1,12 +1,15 @@
 ﻿using Blog.Model.Models.Concrete;
+using Blog.Model.TypeConfigurations.Abstract;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Model.TypeConfigurations.Concrete
 {
-    public class CommentMap:IEntityTypeConfiguration<Comment>
+    public class CommentMap : BaseMap<Comment>
     {
+        
     }
 }
