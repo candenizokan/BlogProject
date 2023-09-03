17666df6ce87e3bff02ac18fba2cc31ca0566fb6
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 13:32:17 +0300

    ProjectContext:IdentityDbContext<AppUser> add reference blog.appuser

--- Blog.Dal/Context/ProjectContext.cs
+++ Blog.Dal/Context/ProjectContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Blog.Model.Models.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Dal.Context
 {
-    public class ProjectContext:IdentityDbContext
+    public class ProjectContext:IdentityDbContext<AppUser>
     {
     }
 }
