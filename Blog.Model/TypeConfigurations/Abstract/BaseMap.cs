0097ee928c94e5fe2edd73aae922bf6af47e43dc
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 12:11:12 +0300

    public abstract class BaseMap<T>:IEntityTypeConfiguration<T> where T:BaseEntity

--- Blog.Model/TypeConfigurations/Abstract/BaseMap.cs
+++ Blog.Model/TypeConfigurations/Abstract/BaseMap.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Blog.Model.Models.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Model.TypeConfigurations.Abstract
 {
-    public abstract class BaseMap
+    public abstract class BaseMap<T>:IEntityTypeConfiguration<T> where T:BaseEntity
     {
     }
 }
