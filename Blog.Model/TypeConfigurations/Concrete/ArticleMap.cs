1fd9bc84670a7f44c1d24eee2fa80df8051e9b7b
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 12:22:25 +0300

    ArticleMap : Title,Content zorunlu alanlar

--- Blog.Model/TypeConfigurations/Concrete/ArticleMap.cs
+++ Blog.Model/TypeConfigurations/Concrete/ArticleMap.cs
@@ -1,12 +1,27 @@
 ﻿using Blog.Model.Models.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Model.TypeConfigurations.Concrete
 {
-    public class ArticleMap: IEntityTypeConfiguration<Article>//Article için db configurasyonu
+    public class ArticleMap : IEntityTypeConfiguration<Article>//Article için db configurasyonu
     {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.Property(a => a.Title).IsRequired(true);
+            builder.Property(a => a.Content).IsRequired(true);
+
+            //navigation prop
+
+
+            //category
+
+
+
+            //user
+        }
     }
 }
