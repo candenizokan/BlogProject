aa4cab4f4e03f265d2af6ab1b2fe43829947f380
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 13:12:17 +0300

    public class UserFollewedCategoryMap: IEntityTypeConfiguration<UserFollewedCategory>

---
+++ Blog.Model/TypeConfigurations/Concrete/UserFollewedCategoryMap.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Model.Models.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Model.TypeConfigurations.Concrete
+{
+    public class UserFollewedCategoryMap: IEntityTypeConfiguration<UserFollewedCategory>
+    {
+    }
+}
