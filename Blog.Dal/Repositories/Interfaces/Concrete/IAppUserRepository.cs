5e8ef7e95665334a94925054c9b624c9da990ef9
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 14:01:36 +0300

    IAppUserRepository

--- Blog.Dal/Repositories/Interfaces/Concrete/IAppUserRepository.cs
+++ Blog.Dal/Repositories/Interfaces/Concrete/IAppUserRepository.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Blog.Model.Models.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Blog.Dal.Repositories.Interfaces.Concrete
 {
     public interface IAppUserRepository
     {
+        //usermanagera ben iş yaptıracağım o yüzden void create metod yazamıyorum. usermanager sınıfı asnkron çalışıyor o yüzden task yazıyorum
+        Task Create(AppUser appUser);
+        Task Delete(AppUser appUser);
+        Task Update(AppUser appUser);
     }
 }
