235c593fba9808f6b5c02ee910259da0f3d05296
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 13:22:52 +0300

    IdentityRole ilk kullanıcı kayıt olurken rolünü belirledik.

--- Blog.Model/TypeConfigurations/Concrete/IdentiyRoleMap.cs
+++ Blog.Model/TypeConfigurations/Concrete/IdentiyRoleMap.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Model.TypeConfigurations.Concrete
 {
-    public class IdentiyRoleMap: IEntityTypeConfiguration<IdentityRole>// db ayağa kalkarken member rolünü oluşturmak istiyorum
+    public class IdentiyRoleMap : IEntityTypeConfiguration<IdentityRole>// db ayağa kalkarken member rolünü oluşturmak istiyorum
     {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(new IdentityRole { Id=Guid.NewGuid().ToString(),Name="Member",NormalizedName="MEMBER" });//kayıtlı kullanıcıya bu rolü atıyorum. kullanıcı kayıt olduğunda rolü meber oluyor. daha sonra rol değişikliğini admin yapıyor.
+        }
     }
 }
