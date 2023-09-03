0fd8cd06a6a849cff086d3fc7d8382bcfa6cd953
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 12:13:55 +0300

    Configure

--- Blog.Model/TypeConfigurations/Abstract/BaseMap.cs
+++ Blog.Model/TypeConfigurations/Abstract/BaseMap.cs
@@ -1,12 +1,17 @@
 ﻿using Blog.Model.Models.Abstract;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Model.TypeConfigurations.Abstract
 {
-    public abstract class BaseMap<T>:IEntityTypeConfiguration<T> where T:BaseEntity
+    public abstract class BaseMap<T> : IEntityTypeConfiguration<T> where T : BaseEntity//BaseEntityden kalıtım alanlar alanların propları konfigure ediyorum. bunu kullansın isterse kendide bir şey yazabiliyor olsun dediğim noktadan Configure'yi virtual yapmam lazım
     {
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.HasKey(a=>a.ID);//primary key 
+        }
     }
 }
