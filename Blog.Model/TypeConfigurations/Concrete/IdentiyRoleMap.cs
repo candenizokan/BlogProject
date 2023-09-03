87dd680e4f6b2b517ec5bd5d88daae03fd73d287
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 13:20:28 +0300

    IdentiyRoleMap: IEntityTypeConfiguration<IdentityRole>

---
+++ Blog.Model/TypeConfigurations/Concrete/IdentiyRoleMap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Model.TypeConfigurations.Concrete
+{
+    public class IdentiyRoleMap: IEntityTypeConfiguration<IdentityRole>// db ayağa kalkarken member rolünü oluşturmak istiyorum
+    {
+    }
+}
