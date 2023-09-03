086446632e05bbc35a0c97e03e6715d03256a00f
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 11:47:00 +0300

    navigation Props Category

--- Blog.Model/Models/Concrete/Category.cs
+++ Blog.Model/Models/Concrete/Category.cs
@@ -1,8 +1,19 @@
 ﻿using Blog.Model.Models.Abstract;
+using System.Collections.Generic;
 
 namespace Blog.Model.Models.Concrete
 {
     public class Category : BaseEntity
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //navigation Prop
+
+        //1 kategorinin çokça makalesi olabilir
+        public List<Article> Articles { get; set; }
+
+        //1 kategoriyi takip eden çokça kişi olabilir.//usera direkt gidemiyorum ara tabloya gitmem lazım
+        public List<UserFollewedCategory> UserFollewedCategories { get; set; }
     }
 }
