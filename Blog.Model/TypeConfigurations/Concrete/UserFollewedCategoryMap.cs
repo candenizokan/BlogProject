5a0162bc3d612c36914095985855c546cf2dfdf6
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 13:13:23 +0300

    UserFollewedCategory pk

--- Blog.Model/TypeConfigurations/Concrete/UserFollewedCategoryMap.cs
+++ Blog.Model/TypeConfigurations/Concrete/UserFollewedCategoryMap.cs
@@ -1,12 +1,18 @@
 ﻿using Blog.Model.Models.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Model.TypeConfigurations.Concrete
 {
-    public class UserFollewedCategoryMap: IEntityTypeConfiguration<UserFollewedCategory>
+    public class UserFollewedCategoryMap : IEntityTypeConfiguration<UserFollewedCategory>
     {
+        public void Configure(EntityTypeBuilder<UserFollewedCategory> builder)
+        {
+            //ikisi beraberce anahtardır.
+            builder.HasKey(a => new { a.AppUserID, a.CategoryID });
+        }
     }
 }
