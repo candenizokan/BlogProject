8a8c92f189438521eba441885b5f7976203d8ca6
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 12:07:19 +0300

    UserFollewedCategory composite key

--- Blog.Model/Models/Concrete/UserFollewedCategory.cs
+++ Blog.Model/Models/Concrete/UserFollewedCategory.cs
@@ -2,5 +2,13 @@
 {
     public class UserFollewedCategory
     {
+        //Bu somof ara tablo sınıfıdır. buradaki id ler hem foreign key hem primary key yani composite key olacaktır. Bu yüzden bir kez daha BaseEntityden kalıtım almasına gerek yoktur
+
+
+        public string AppUserID { get; set; }
+        public AppUser AppUser { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
     }
 }
