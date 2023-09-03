3f5a1a9e78b14024cdc976391a784c54617b895a
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 13:39:06 +0300

    ProjectContext=connection string/// DBSET//Configuration(MAPS)

--- Blog.Dal/Context/ProjectContext.cs
+++ Blog.Dal/Context/ProjectContext.cs
@@ -1,12 +1,47 @@
 ﻿using Blog.Model.Models.Concrete;
+using Blog.Model.TypeConfigurations.Concrete;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Dal.Context
 {
-    public class ProjectContext:IdentityDbContext<AppUser>
+    public class ProjectContext : IdentityDbContext<AppUser>
     {
+        //connection string ne
+        //hangi tablo
+        //konfigurasyonların ne
+
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options) //DbContextOptions parametresi alacaksın ve base'ine atana göndereceksin
+        {
+
+        }
+
+        //hangi tablo DBSET
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<UserFollewedCategory> UserFollewedCategories { get; set; }
+
+        //konfigurasyonlarım yani tablolar ayağa kalkarken neye göre kalksın. maplerimi göndereceğim
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new AppUserMap());
+            builder.ApplyConfiguration(new ArticleMap());
+            builder.ApplyConfiguration(new CommentMap());
+            builder.ApplyConfiguration(new LikeMap());
+            builder.ApplyConfiguration(new UserFollewedCategoryMap());
+            builder.ApplyConfiguration(new IdentiyRoleMap());
+
+
+            base.OnModelCreating(builder);
+        }
+
     }
 }
