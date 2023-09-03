5a05402a0ff66a4f4bf23e8bbea4c9bb21f94e96
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 13:11:04 +0300

    LikeMap foreign key and navigation prop

--- Blog.Model/TypeConfigurations/Concrete/LikeMap.cs
+++ Blog.Model/TypeConfigurations/Concrete/LikeMap.cs
@@ -1,12 +1,22 @@
 ﻿using Blog.Model.Models.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Model.TypeConfigurations.Concrete
 {
-    public class LikeMap: IEntityTypeConfiguration<Like>
+    public class LikeMap : IEntityTypeConfiguration<Like>
     {
+        public void Configure(EntityTypeBuilder<Like> builder)
+        {
+            builder.HasKey(a => new { a.AppUserID, a.ArticleID });
+
+            builder.HasOne(a => a.AppUser).WithMany(a => a.Likes).HasForeignKey(a => a.AppUserID);
+
+            //abir like bir makalede olur. makalenin çokça beğenisi olabilir- bunlar birbirine bağlıdır articleid üzerinden
+            builder.HasOne(a => a.Article).WithMany(a => a.Likes).HasForeignKey(a => a.ArticleID);
+        }
     }
 }
