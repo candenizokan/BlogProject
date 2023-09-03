6a9a860e7ecafdcaacca6e88e8987349da4a47ff
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 11:55:41 +0300

    Comment navigation prop

--- Blog.Model/Models/Concrete/Comment.cs
+++ Blog.Model/Models/Concrete/Comment.cs
@@ -8,5 +8,13 @@ namespace Blog.Model.Models.Concrete
         public string Text { get; set; }
 
         //navigation prop
+
+        //1 yorum 1 kişinindir. ////appuser nesnesi ve yazar appuser tutmam lazım
+        public string AppUserID { get; set; }
+        public AppUser AppUser { get; set; }
+
+        //1 yorum 1 makaleye aittir.//Article nesnesi ve Article tutmam lazım
+        public int ArticleID { get; set; }
+        public Article Article { get; set; }
     }
 }
