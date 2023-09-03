b3708a18477dcb8f50e21628bdf8eb9f06bc6ec5
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 13:17:09 +0300

    UserFollewedCategory navigationprop

--- Blog.Model/TypeConfigurations/Concrete/UserFollewedCategoryMap.cs
+++ Blog.Model/TypeConfigurations/Concrete/UserFollewedCategoryMap.cs
@@ -13,6 +13,13 @@ namespace Blog.Model.TypeConfigurations.Concrete
         {
             //ikisi beraberce anahtardır.
             builder.HasKey(a => new { a.AppUserID, a.CategoryID });
+
+            //navigation prop
+            //bir ara tablounun bir userı vardıdr
+            builder.HasOne(a => a.AppUser).WithMany(a => a.UserFollewedCategories).HasForeignKey(a => a.AppUserID);
+
+            //
+            builder.HasOne(a => a.Category).WithMany(a => a.UserFollewedCategories).HasForeignKey(a => a.CategoryID);
         }
     }
 }
