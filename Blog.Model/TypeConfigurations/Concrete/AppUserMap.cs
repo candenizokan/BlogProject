2819e6594ddd0c97db14f57589b1f75598a7f11e
Okan Candeniz <candenizokan@gmail.com>
2023-09-03 12:18:22 +0300

    FirstName,LastName zorunlu olsun

--- Blog.Model/TypeConfigurations/Concrete/AppUserMap.cs
+++ Blog.Model/TypeConfigurations/Concrete/AppUserMap.cs
@@ -1,12 +1,18 @@
 ﻿using Blog.Model.Models.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Model.TypeConfigurations.Concrete
 {
-    public class AppUserMap: IEntityTypeConfiguration<AppUser>//AppUser için config yapıyorum
+    public class AppUserMap : IEntityTypeConfiguration<AppUser>//AppUser için config yapıyorum
     {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(a => a.FirstName).IsRequired(true);
+            builder.Property(a => a.LastName).IsRequired(true);
+        }
     }
 }
