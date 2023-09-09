526a06145676b465ff331c537ea07227a5507e60
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 11:00:54 +0300

    ctor _context

--- Blog.Dal/Repositories/Concrete/LikeRepository.cs
+++ Blog.Dal/Repositories/Concrete/LikeRepository.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+﻿using Blog.Dal.Context;
+using Blog.Dal.Repositories.Interfaces.Concrete;
 using Blog.Model.Models.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,12 @@ namespace Blog.Dal.Repositories.Concrete
 {
     public class LikeRepository : ILikeRepository
     {
+        private readonly ProjectContext _context;
+
+        public LikeRepository(ProjectContext context)
+        {
+            _context = context;
+        }
         public void Create(Like like)
         {
             throw new NotImplementedException();
