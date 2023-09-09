cf5a8097750b2469c7dfd011517cef50dc59905f
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 10:21:42 +0300

    note

--- Blog.Dal/Repositories/Abstract/BaseRepository.cs
+++ Blog.Dal/Repositories/Abstract/BaseRepository.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Blog.Dal.Repositories.Abstract
 {
-    public class BaseRepository<T> :IBaseRepository<T> where T:BaseEntity
+    public class BaseRepository<T> :IBaseRepository<T> where T: BaseEntity//BaseEntity den ne tür gelirse gelsin tek bir base repository comet,category,article gelse crud yapacaksın
     {
     }
 }
