fadfbf3212bbe63d05771fa37fae0344a3a85c33
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 11:01:41 +0300

    _table= _context.Set<Like>();

--- Blog.Dal/Repositories/Concrete/LikeRepository.cs
+++ Blog.Dal/Repositories/Concrete/LikeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Dal.Context;
 using Blog.Dal.Repositories.Interfaces.Concrete;
 using Blog.Model.Models.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,10 +11,12 @@ namespace Blog.Dal.Repositories.Concrete
     public class LikeRepository : ILikeRepository
     {
         private readonly ProjectContext _context;
+        private readonly DbSet<Like> _table;
 
         public LikeRepository(ProjectContext context)
         {
             _context = context;
+            _table= _context.Set<Like>();
         }
         public void Create(Like like)
         {
