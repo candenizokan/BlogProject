3701a38e3058984a232fd92ca234cf926c2ecdca
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 13:51:36 +0300

    LoginDTO props

---
+++ Blog.Web/Models/DTOs/LoginDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Web.Models.DTOs
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "Bu alan Boş Bırakılamaz")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Şifrenizi Yazınız")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        //bu alanı gette gitmek istediği bir yer varsa doldurup göndereceğiz ki posttaki işlemler doğruysa buradaki url üzerinden doğrudan gitmek istediği yere yönlendirelim.
+
+        //aynı kişiyi update ederken id bilgisini postta kullanmak üzere gömmemiz gibi
+        public string ReturnUrl { get; set; }
+    }
+}
