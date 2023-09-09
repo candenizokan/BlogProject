58ddcfc14574a41f4de8daf3db64d5b99e52d353
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 11:06:57 +0300

    implement interface

--- Blog.Dal/Repositories/Concrete/AppUserRepository.cs
+++ Blog.Dal/Repositories/Concrete/AppUserRepository.cs
@@ -1,11 +1,27 @@
 ﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using Blog.Model.Models.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Blog.Dal.Repositories.Concrete
 {
     public class AppUserRepository : IAppUserRepository
     {
+        public Task Create(AppUser appUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(AppUser appUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(AppUser appUser)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
