f6d04f6464f9b21f6ad6f975a1fb39bf6ae2f18b
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 12:19:57 +0300

    prop RegisterDTO

--- Blog.Web/Models/DTOs/RegisterDTO.cs
+++ Blog.Web/Models/DTOs/RegisterDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace Blog.Web.Models.DTOs
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Blog.Web.Models.DTOs
 {
     public class RegisterDTO
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public IFormFile Image { get; set; }
     }
 }
