ab127ce12d9470964b381319e6d1e35d7edc2bb1
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 10:20:18 +0300

    public class BaseRepository<T> :IBaseRepository<T> where T:BaseEntity

--- Blog.Dal/Repositories/Abstract/BaseRepository.cs
+++ Blog.Dal/Repositories/Abstract/BaseRepository.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Blog.Dal.Repositories.Interfaces.Abstract;
+using Blog.Model.Models.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Dal.Repositories.Abstract
 {
-    public class BaseRepository
+    public class BaseRepository<T> :IBaseRepository<T> where T:BaseEntity
     {
     }
 }
