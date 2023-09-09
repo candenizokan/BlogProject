e3d09f49f34f97ff0b3dbc471224b02e6c33de87
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 12:53:50 +0300

    AppUser appUser = _mapper.Map<AppUser>(dto);

--- Blog.Web/Controllers/AccountController.cs
+++ Blog.Web/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace Blog.Web.Controllers
             {
                 //aoutomapperdan destek alacağım
 
-                AppUser appUser=
+                AppUser appUser = _mapper.Map<AppUser>(dto);
             }
             return View(dto);
         }
