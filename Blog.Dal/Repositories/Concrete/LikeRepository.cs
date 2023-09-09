252ff548d498fb349c3ca83ad348d0a3da798dc5
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 11:00:08 +0300

    implement interface

--- Blog.Dal/Repositories/Concrete/LikeRepository.cs
+++ Blog.Dal/Repositories/Concrete/LikeRepository.cs
@@ -1,11 +1,21 @@
 ﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using Blog.Model.Models.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Dal.Repositories.Concrete
 {
-    public class LikeRepository:ILikeRepository
+    public class LikeRepository : ILikeRepository
     {
+        public void Create(Like like)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Like like)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
