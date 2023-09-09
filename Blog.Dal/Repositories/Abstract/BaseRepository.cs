7cb95866e026fedd0fc2daa4a3ce23f8f08ba318
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 10:21:54 +0300

    implement interface

--- Blog.Dal/Repositories/Abstract/BaseRepository.cs
+++ Blog.Dal/Repositories/Abstract/BaseRepository.cs
@@ -1,12 +1,49 @@
 ﻿using Blog.Dal.Repositories.Interfaces.Abstract;
 using Blog.Model.Models.Abstract;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Blog.Dal.Repositories.Abstract
 {
-    public class BaseRepository<T> :IBaseRepository<T> where T: BaseEntity//BaseEntity den ne tür gelirse gelsin tek bir base repository comet,category,article gelse crud yapacaksın
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity//BaseEntity den ne tür gelirse gelsin tek bir base repository comet,category,article gelse crud yapacaksın
     {
+        public void Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult GetByDefault<TResult>(Expression<Func<T, TResult>> selector, Expression<Func<T, bool>> expression, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TResult> GetByDefaults<TResult>(Expression<Func<T, TResult>> selector, Expression<Func<T, bool>> expression, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetDefault(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetDefaults(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
