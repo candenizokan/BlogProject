0fa721a297beffa0a09e57869f047014859c14b9
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 11:06:44 +0300

    public class AppUserRepository : IAppUserRepository

--- Blog.Dal/Repositories/Concrete/AppUserRepository.cs
+++ Blog.Dal/Repositories/Concrete/AppUserRepository.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Dal.Repositories.Concrete
 {
-    public class AppUserRepository
+    public class AppUserRepository : IAppUserRepository
     {
     }
 }
