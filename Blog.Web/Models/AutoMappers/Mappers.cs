54c647dcc5c5ad8bf40410f42e57da7edb4d701c
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 12:48:11 +0300

    CreateMap<RegisterDTO, AppUser>();

--- Blog.Web/Models/AutoMappers/Mappers.cs
+++ Blog.Web/Models/AutoMappers/Mappers.cs
@@ -1,6 +1,16 @@
-﻿namespace Blog.Web.Models.AutoMappers
+﻿using AutoMapper;
+using Blog.Model.Models.Concrete;
+using Blog.Web.Models.DTOs;
+
+namespace Blog.Web.Models.AutoMappers
 {
-    public class Mappers
+    public class Mappers:Profile
     {
+        public Mappers()
+        {
+            //mapplemeler
+
+            CreateMap<RegisterDTO, AppUser>();//sen bana registerDTO dan bir appuser nesnesi teslim etmelisin
+        }
     }
 }
