1451dc4dc11cfef308246c313eeb430ed74cfce3
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 10:53:49 +0300

    ctor CategoryRepository

--- Blog.Dal/Repositories/Concrete/CategoryRepository.cs
+++ Blog.Dal/Repositories/Concrete/CategoryRepository.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Blog.Dal.Context;
+using Blog.Dal.Repositories.Abstract;
+using Blog.Model.Models.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Dal.Repositories.Concrete
 {
-    public class CategoryRepository
+    public class CategoryRepository : BaseRepository<Category>
     {
+        public CategoryRepository(ProjectContext context) : base(context)
+        {
+        }
     }
 }
