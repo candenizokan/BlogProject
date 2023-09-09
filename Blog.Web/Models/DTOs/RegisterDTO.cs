871a3190335f237cb353e86ff67eeb65297ac676
Okan Candeniz <candenizokan@gmail.com>
2023-09-09 12:23:05 +0300

    RegisterDTO DataAnnotations

--- Blog.Web/Models/DTOs/RegisterDTO.cs
+++ Blog.Web/Models/DTOs/RegisterDTO.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Web.Models.DTOs
 {
     public class RegisterDTO
     {
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
         public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Bu alan boş bırakılamaz")]
+        [NotMapped]     //bu property mapleme, eşleştirme yapma !
         public IFormFile Image { get; set; }
     }
 }
