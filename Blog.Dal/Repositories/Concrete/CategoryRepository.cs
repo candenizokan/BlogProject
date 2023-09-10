9f8e8e581d5a3d7ddcb864b18912e57762eca7cd
Okan Candeniz <candenizokan@gmail.com>
2023-09-10 12:33:46 +0300

    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository

--- Blog.Dal/Repositories/Concrete/CategoryRepository.cs
+++ Blog.Dal/Repositories/Concrete/CategoryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Blog.Dal.Context;
 using Blog.Dal.Repositories.Abstract;
+using Blog.Dal.Repositories.Interfaces.Concrete;
 using Blog.Model.Models.Concrete;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Text;
 
 namespace Blog.Dal.Repositories.Concrete
 {
-    public class CategoryRepository : BaseRepository<Category>
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
     {
         public CategoryRepository(ProjectContext context) : base(context)
         {
