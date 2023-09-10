3eeacad2baf08ce33aa5dba45bcc84a027b865bb
Okan Candeniz <candenizokan@gmail.com>
2023-09-10 12:33:15 +0300

    public class ArticleRepository : BaseRepository<Article>,IArticleRepository

--- Blog.Dal/Repositories/Concrete/ArticleRepository.cs
+++ Blog.Dal/Repositories/Concrete/ArticleRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Blog.Dal.Context;
 using Blog.Dal.Repositories.Abstract;
+using Blog.Dal.Repositories.Interfaces.Concrete;
 using Blog.Model.Models.Concrete;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Text;
 
 namespace Blog.Dal.Repositories.Concrete
 {
-    public class ArticleRepository : BaseRepository<Article>
+    public class ArticleRepository : BaseRepository<Article>,IArticleRepository
     {
         public ArticleRepository(ProjectContext context) : base(context)//context alacam ben kullanmayacağım atam base'e göndereceğim yani baserepository'e yolladım
         {
