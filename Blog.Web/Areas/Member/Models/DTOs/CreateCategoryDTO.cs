1ea35b7ffeb2f85b32aa2ba642ff04d7f28b0795
Okan Candeniz <candenizokan@gmail.com>
2023-09-10 11:59:31 +0300

    CreateCategoryDTO DataAnnotations

--- Blog.Web/Areas/Member/Models/DTOs/CreateCategoryDTO.cs
+++ Blog.Web/Areas/Member/Models/DTOs/CreateCategoryDTO.cs
@@ -1,9 +1,15 @@
-﻿namespace Blog.Web.Areas.Member.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Web.Areas.Member.Models.DTOs
 {
     public class CreateCategoryDTO
     {
+        [Required(ErrorMessage = "Bu alan Boş Bırakılamaz")]
+        [MinLength(2, ErrorMessage = "En az 2 karakter yazmalısınız")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Bu alan Boş Bırakılamaz")]
+        [MinLength(10, ErrorMessage = "En az 10 karakter yazmalısınız")]
         public string Description { get; set; }
     }
 }
