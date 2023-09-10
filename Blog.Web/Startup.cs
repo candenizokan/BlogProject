3b69def75b5c835dbb6080400148db394d2c5b27
Okan Candeniz <candenizokan@gmail.com>
2023-09-10 12:36:05 +0300

    services.AddScoped<ICategoryRepository, CategoryRepository>();

--- Blog.Web/Startup.cs
+++ Blog.Web/Startup.cs
@@ -53,7 +53,7 @@ namespace Blog.Web
             services.AddAutoMapper(typeof(Mappers));
 
             services.AddScoped<IAppUserRepository, AppUserRepository>();
-            services.AddScoped<IBaseRepository<Category>, CategoryRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
