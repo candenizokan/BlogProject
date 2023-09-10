edff5ec30036c1531954ce9eeb63cddc4e9ac16d
Okan Candeniz <candenizokan@gmail.com>
2023-09-10 13:28:57 +0300

    UpdateCategoryDTO props

---
+++ Blog.Web/Areas/Member/Models/DTOs/UpdateCategoryDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Web.Areas.Member.Models.DTOs
+{
+    public class UpdateCategoryDTO
+    {
+        //createte hangisini açtıysam update de de aynını açmalıyım
+
+        [Required(ErrorMessage = "Bu alan Boş Bırakılamaz")]
+        [MinLength(2, ErrorMessage = "En az 2 karakter yazmalısınız")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Bu alan Boş Bırakılamaz")]
+        [MinLength(10, ErrorMessage = "En az 10 karakter yazmalısınız")]
+        public string Description { get; set; }
+
+        public int ID { get; set; }
+        //posta düştüğümde kimin güncelleyeceğimi bilmesi için ID si muhakkak olmalı
+    }
+}
