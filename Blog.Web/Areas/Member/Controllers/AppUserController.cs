f4635066895929fbbdbcc4693c2ed7b8dc4fb647
Okan Candeniz <candenizokan@gmail.com>
2023-09-10 11:48:44 +0300

    _signInManager ctor di

--- Blog.Web/Areas/Member/Controllers/AppUserController.cs
+++ Blog.Web/Areas/Member/Controllers/AppUserController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Model.Models.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Member.Controllers
 {
     [Area("Member")]
     public class AppUserController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AppUserController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
