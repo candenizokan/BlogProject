cecef6bc59f79923112e3d3a8e87576b2b981a12
Okan Candeniz <candenizokan@gmail.com>
2023-09-10 12:14:55 +0300

    _mapper

--- Blog.Web/Areas/Member/Controllers/CategoryController.cs
+++ Blog.Web/Areas/Member/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Web.Areas.Member.Models.DTOs;
+﻿using AutoMapper;
+using Blog.Web.Areas.Member.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Member.Controllers
@@ -6,6 +7,12 @@ namespace Blog.Web.Areas.Member.Controllers
     [Area("Member")]
     public class CategoryController : Controller
     {
+        private readonly IMapper _mapper;
+
+        public CategoryController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
         public IActionResult Create()
         {
             return View();
