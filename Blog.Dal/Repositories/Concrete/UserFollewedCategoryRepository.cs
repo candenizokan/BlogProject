fc30b331984197affe8c23584d2d0973445a4337
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-16 10:40:50 +0300

    implement interface

--- Blog.Dal/Repositories/Concrete/UserFollewedCategoryRepository.cs
+++ Blog.Dal/Repositories/Concrete/UserFollewedCategoryRepository.cs
@@ -1,11 +1,22 @@
 ﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using Blog.Model.Models.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Blog.Dal.Repositories.Concrete
 {
-    public class UserFollewedCategoryRepository: IUserFollewedCategoryRepository
+    public class UserFollewedCategoryRepository : IUserFollewedCategoryRepository
     {
+        public void Delete(UserFollewedCategory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<UserFollewedCategory> GetFollowedCategories(Expression<Func<UserFollewedCategory, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
