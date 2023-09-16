e01190503ffaf30e3e86586185ab065ad24d6f92
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-16 12:20:39 +0300

    _userManager

--- Blog.Web/Areas/Member/Controllers/ArticleController.cs
+++ Blog.Web/Areas/Member/Controllers/ArticleController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Model.Models.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Member.Controllers
 {
     public class ArticleController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public ArticleController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Create()
         {
+            //içerdeki kişiyi bulayım. user managera ihtiyacım var . di ile alacağım
             return View();
         }
     }
