900f8540e4f1343de48f1672adc6b212d4fba5b5
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-16 13:57:21 +0300

    GetArticleVM

---
+++ Blog.Web/Areas/Member/Models/VMs/GetArticleVM.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.Web.Areas.Member.Models.VMs
+{
+    public class GetArticleVM//listeleme sayfasına bunları göstermek istiyorum. serverdan kullanıcıya geliyor. posta düşmeyecek. validasyon kontrolüne gerek yok
+    {
+        //Article
+
+        public int ArticleID { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+
+        //Category
+        public string CategoryName { get; set; }
+
+        //user
+        public string FullName { get; set; }
+    }
+}
