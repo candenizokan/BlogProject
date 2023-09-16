05911d24835162a5dd9859dcdee00c1f5ba25d0f
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-16 10:36:24 +0300

    IUserFollewedCategoryRepository metods

--- Blog.Dal/Repositories/Concrete/IUserFollewedCategoryRepository.cs
+++ Blog.Dal/Repositories/Concrete/IUserFollewedCategoryRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Blog.Model.Models.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Blog.Dal.Repositories.Concrete
 {
     public interface IUserFollewedCategoryRepository// baseden kalıtım almadığından IBaseRepositoryden implemantasyon yapmaz.
     {
+        void Delete(UserFollewedCategory entity);
+
+        List<UserFollewedCategory> GetFollowedCategories(Expression<Func<UserFollewedCategory,bool>> expression);
     }
 }
