c8f7b1823fe22113eb0481e8273b00badcc52394
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-16 10:40:36 +0300

    public class UserFollewedCategoryRepository: IUserFollewedCategoryRepository

---
+++ Blog.Dal/Repositories/Concrete/UserFollewedCategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Dal.Repositories.Concrete
+{
+    public class UserFollewedCategoryRepository: IUserFollewedCategoryRepository
+    {
+    }
+}
