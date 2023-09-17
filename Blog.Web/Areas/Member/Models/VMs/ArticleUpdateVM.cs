8415d0fad7e178355e362e4b188ca8f718cccdbf
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-17 10:45:38 +0300

    ArticleUpdateVM

--- Blog.Web/Areas/Member/Models/VMs/ArticleUpdateVM.cs
+++ Blog.Web/Areas/Member/Models/VMs/ArticleUpdateVM.cs
@@ -1,6 +1,29 @@
-﻿namespace Blog.Web.Areas.Member.Models.VMs
+﻿using Blog.Web.Areas.Member.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Web.Areas.Member.Models.VMs
 {
     public class ArticleUpdateVM
     {
+        //Article
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+
+        //Category
+
+        public int CategoryID { get; set; }//seçili olan kategory
+
+        public List<GetCategoryDTO> Categories { get; set; }
+
+        //USER
+
+        public string AppUserID { get; set; }
     }
 }
