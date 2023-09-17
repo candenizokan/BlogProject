c42d59bc97bfc54595632401558f8cde0bbd1e7f
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-17 12:32:41 +0300

    ArticleDetailVM

--- Blog.Web/Areas/Member/Models/VMs/ArticleDetailVM.cs
+++ Blog.Web/Areas/Member/Models/VMs/ArticleDetailVM.cs
@@ -1,7 +1,37 @@
-﻿namespace Blog.Web.Areas.Member.Models.VMs
+﻿
+
+using Blog.Model.Models.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+
+namespace Blog.Web.Areas.Member.Models.VMs
 {
     public class ArticleDetailVM
     {
         //Article
+        public int ArticleID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+        public string Image { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Like> Likes { get; set; }
+
+
+        //Category
+
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+        //APPuser
+
+        public string UserID { get; set; }
+        public string UserFullNamel { get; set; }
+        public string UserImage { get; set; }
+        public DateTime UserCreatedDate { get; set; }
+
+        // toDo: comment=> ???
     }
 }
