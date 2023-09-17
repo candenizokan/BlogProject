157458487d9e4a09533a9e6aae3968993bf6e6bd
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-17 10:48:57 +0300

    ArticleUpdateVM props

--- Blog.Web/Areas/Member/Models/VMs/ArticleUpdateVM.cs
+++ Blog.Web/Areas/Member/Models/VMs/ArticleUpdateVM.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Web.Areas.Member.Models.DTOs;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Web.Areas.Member.Models.VMs
@@ -9,21 +10,28 @@ namespace Blog.Web.Areas.Member.Models.VMs
     {
         //Article
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Başlık Bilgisi Boş Bırakılamaz")]
+        [MinLength(2, ErrorMessage = "En az 2 karakter yazmalısınız")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "İçerik Bilgisi Boş Bırakılamaz")]
+        [MinLength(200, ErrorMessage = "İçerik En az 200 karakter yazmalısınız")]
         public string Content { get; set; }
 
-        [NotMapped]
+        [NotMapped] // eşleşme yapma
+        [Required(ErrorMessage = "Lütfen Fotoğraf Seçiniz")]
         public IFormFile Image { get; set; }
 
 
         //Category
-
+        [Required(ErrorMessage = "Lütfen Kategori Seçiniz")]
         public int CategoryID { get; set; }//seçili olan kategory
 
         public List<GetCategoryDTO> Categories { get; set; }
 
         //USER
-
+        [Required]
         public string AppUserID { get; set; }
     }
 }
