0c0196d69cdacf5bd61a52b0c80cd256fdc9c430
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-23 12:07:36 +0300

    note

--- Blog.Web/Views/Shared/Components/ArticlesWith/ArticlesWithViewComponent.cs
+++ Blog.Web/Views/Shared/Components/ArticlesWith/ArticlesWithViewComponent.cs
@@ -10,7 +10,7 @@ namespace Blog.Web.Views.Shared.Components.ArticlesWith
         public IViewComponentResult Invoke()
         {
 
-
+            //makalenin her şeyini veya bilgilerini getirmeyeceğim categorinin bzaı datalarını alacağım. list article almayacağım. neyi istiyorsam vm nesnesi ile almam lazım. şu şu dataları alacağım. hangi propları taşıyacağımı vm üzerindenn karar vereceğim
 
 
         }
