91757edbd68f5d886ceb3cf0020eb876620a7fe0
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-23 12:19:19 +0300

    _articleRepository

--- Blog.Web/Views/Shared/Components/ArticlesWith/ArticlesWithViewComponent.cs
+++ Blog.Web/Views/Shared/Components/ArticlesWith/ArticlesWithViewComponent.cs
@@ -1,16 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Views.Shared.Components.ArticlesWith
 {
     public class ArticlesWithViewComponent : ViewComponent
     {
-
         //En güncel 10 makaleyi getirelim
+        private readonly IArticleRepository _articleRepository;
 
+        
+
+
+        public ArticlesWithViewComponent(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
         public IViewComponentResult Invoke()
         {
 
             //makalenin her şeyini veya bilgilerini getirmeyeceğim categorinin bzaı datalarını alacağım. list article almayacağım. neyi istiyorsam vm nesnesi ile almam lazım. şu şu dataları alacağım. hangi propları taşıyacağımı vm üzerindenn karar vereceğim
+            //aktif makaleleri getir. sırala. 
+            //article repoya itiyacım var di ile alacam
+
+
 
 
         }
