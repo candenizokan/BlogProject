51db574b6ada2fedfaf170ddea21f26c6e140acc
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-23 13:59:30 +0300

    _articleRepository

--- Blog.Web/Controllers/ArticleController.cs
+++ Blog.Web/Controllers/ArticleController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
     public class ArticleController : Controller
     {
+        private readonly IArticleRepository _articleRepository;
+
+        public ArticleController(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
         public IActionResult Detail(int id)//makale id
         {
             return View();
