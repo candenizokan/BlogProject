d0823e08eafb31c15147663a31a51e2e56217ab7
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-23 12:11:30 +0300

    GetArticleVM props

--- Blog.Web/Models/VMs/GetArticleVM.cs
+++ Blog.Web/Models/VMs/GetArticleVM.cs
@@ -1,6 +1,25 @@
-﻿namespace Blog.Web.Models.VMs
+﻿using System;
+
+namespace Blog.Web.Models.VMs
 {
     public class GetArticleVM
     {
+
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int ArticleID { get; set; }
+
+
+        public string ImagePath { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public string AppUserID { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string UserFullName { get; set; }
     }
 }
