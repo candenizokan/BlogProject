574d70654fe1ee72f048a13a3371f214d42ce856
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-23 14:04:04 +0300

    ArticleDetailVM areas dışında

--- Blog.Web/Controllers/ArticleController.cs
+++ Blog.Web/Controllers/ArticleController.cs
@@ -1,5 +1,7 @@
 ﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using Blog.Model.Models.Enums;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Web.Controllers
 {
@@ -13,7 +15,30 @@ namespace Blog.Web.Controllers
         }
         public IActionResult Detail(int id)//makale id
         {
-            return View();
+            //like ve comment taşımıyorum. dışarıda olmayacak. like comment yapanlar içeridekiler olacak
+
+            var article = _articleRepository.GetByDefault
+                (
+                    selector: a => new ArticleDetailVM()//area dışındaki vm'i kullanmalıyım
+                    {
+                        ArticleID = a.ID,
+                        Title = a.Title,
+                        CreatedDate = a.CreatedDate,
+                        Image = a.ImagePath,
+                        Content = a.Content,
+                        Likes = a.Likes,
+                        CategoryID = a.CategoryID,
+                        CategoryName = a.Category.Name,
+                        UserID = a.AppUserID,
+                        UserCreatedDate = a.AppUser.CreatedDate,
+                        UserFullName = a.AppUser.FullName,
+                        UserImage = a.AppUser.ImagePath
+
+                    },
+                    expression: a => a.Statu != Statu.Passive && a.ID == id,
+                    include: a => a.Include(a => a.AppUser).Include(a => a.Category)
+                );
+            return View(article);
         }
     }
 }
