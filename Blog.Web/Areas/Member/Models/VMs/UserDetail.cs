12505c3ba80ca58bba50d47b533fa6f68e6dd815
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-24 12:28:19 +0300

    user detail props

--- Blog.Web/Areas/Member/Models/VMs/UserDetail.cs
+++ Blog.Web/Areas/Member/Models/VMs/UserDetail.cs
@@ -2,5 +2,9 @@
 {
     public class UserDetail
     {
+        public string FullName { get; set; }
+        public string UserID { get; set; }
+        public int AllArticleCount { get; set; }// active + modified +passive article
+        public int AllNotPassiveArticleCount { get; set; } // active + modified article
     }
 }
--- Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
+++ Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
@@ -5,5 +5,7 @@ namespace Blog.Web.Areas.Member.Views.Shared.Components.OldUser
     public class OldUserViewComponent :ViewComponent
     {
         //member areadaki anasayfada yani en eski ilk 4 kullanıcımızın ad, soyad, varsa fullname, id ve bu zamana kadarki toplam makale sayısını(aktif pasif herşey) ve aktif makale sayısını(pasif olmayan) gösteren ufak cad yapısına bu bilgileri basınız
+
+        //userları getirceğim açin appuserrepoya ihtiyacım var. db den herkesi getirmem lazım 
     }
 }
