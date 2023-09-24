c3eb1ca3a83adfe84d978c2bd5e2211d6ace6a70
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-24 11:38:13 +0300

    user

--- Blog.Web/Areas/Member/Views/Shared/Components/FollowedCategory/FollowedCategoryViewComponent.cs
+++ Blog.Web/Areas/Member/Views/Shared/Components/FollowedCategory/FollowedCategoryViewComponent.cs
@@ -2,6 +2,8 @@
 using Blog.Model.Models.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Blog.Web.Areas.Member.Views.Shared.Components.FollowedCategory
 {
@@ -17,6 +19,15 @@ namespace Blog.Web.Areas.Member.Views.Shared.Components.FollowedCategory
         {
             _categoryRepository = categoryRepository;
             _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            AppUser appUser = await _userManager.GetUserAsync((System.Security.Claims.ClaimsPrincipal)User);
+
+            List<Category> list = _categoryRepository.GetCategoryWithId(appUser.Id);
+
+            return View(list);
         }
     }
 }
