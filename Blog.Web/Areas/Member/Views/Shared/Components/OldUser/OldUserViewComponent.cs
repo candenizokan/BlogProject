b3afa20cb37561bae943f52eed157bf87b15a87f
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-24 12:05:10 +0300

    public class OldUserViewComponent :ViewComponent

--- Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
+++ Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
@@ -1,6 +1,8 @@
-﻿namespace Blog.Web.Areas.Member.Views.Shared.Components.OldUser
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Areas.Member.Views.Shared.Components.OldUser
 {
-    public class OldUserViewComponent
+    public class OldUserViewComponent :ViewComponent
     {
     }
 }
