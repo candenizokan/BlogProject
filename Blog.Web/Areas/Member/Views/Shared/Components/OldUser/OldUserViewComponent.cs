5f86f3b08936105cc6503fdc9243e90cb7fc8bff
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-24 12:30:05 +0300

    _userManager

--- Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
+++ Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Model.Models.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Member.Views.Shared.Components.OldUser
 {
@@ -7,5 +9,11 @@ namespace Blog.Web.Areas.Member.Views.Shared.Components.OldUser
         //member areadaki anasayfada yani en eski ilk 4 kullanıcımızın ad, soyad, varsa fullname, id ve bu zamana kadarki toplam makale sayısını(aktif pasif herşey) ve aktif makale sayısını(pasif olmayan) gösteren ufak cad yapısına bu bilgileri basınız
 
         //userları getirceğim açin appuserrepoya ihtiyacım var. db den herkesi getirmem lazım 
+        private readonly UserManager<AppUser> _userManager;
+
+        public OldUserViewComponent(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
     }
 }
