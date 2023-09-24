2dd9896756438d32afa0e386e5c008bad37796b1
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-24 12:07:05 +0300

    note

--- Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
+++ Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
@@ -4,5 +4,6 @@ namespace Blog.Web.Areas.Member.Views.Shared.Components.OldUser
 {
     public class OldUserViewComponent :ViewComponent
     {
+        //member areadaki anasayfada yani en eski ilk 4 kullanıcımızın ad, soyad, varsa fullname, id ve bu zamana kadarki toplam makale sayısını(aktif pasif herşey) ve aktif makale sayısını(pasif olmayan) gösteren ufak cad yapısına bu bilgileri basınız
     }
 }
