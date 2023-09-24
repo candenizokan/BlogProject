df749e1ad42f63a5c868a75c047b10dbc165b94f
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-24 12:47:49 +0300

    public OldUserViewComponent(UserManager<AppUser> userManager)

--- Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
+++ Blog.Web/Areas/Member/Views/Shared/Components/OldUser/OldUserViewComponent.cs
@@ -1,10 +1,14 @@
 ﻿using Blog.Model.Models.Concrete;
+using Blog.Web.Areas.Member.Models.VMs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Blog.Web.Areas.Member.Views.Shared.Components.OldUser
 {
-    public class OldUserViewComponent :ViewComponent
+    public class OldUserViewComponent : ViewComponent
     {
         //member areadaki anasayfada yani en eski ilk 4 kullanıcımızın ad, soyad, varsa fullname, id ve bu zamana kadarki toplam makale sayısını(aktif pasif herşey) ve aktif makale sayısını(pasif olmayan) gösteren ufak cad yapısına bu bilgileri basınız
 
@@ -14,6 +18,22 @@ namespace Blog.Web.Areas.Member.Views.Shared.Components.OldUser
         public OldUserViewComponent(UserManager<AppUser> userManager)
         {
             _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var list = await _userManager.Users
+                        .Where(a => a.Statu != Model.Models.Enums.Statu.Passive)
+                        .OrderBy(a => a.CreatedDate) 
+                        .Select(a => new UserDetail
+                        {
+                            FullName = a.FullName,
+                            UserID = a.Id,
+                            AllArticleCount = a.Articles.Count,
+                            AllNotPassiveArticleCount = a.Articles.Count(article => article.Statu != Model.Models.Enums.Statu.Passive)
+                        }).Take(4).ToListAsync();
+
+            return View(list);
         }
     }
 }
