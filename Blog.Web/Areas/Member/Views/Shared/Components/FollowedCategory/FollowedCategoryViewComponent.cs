89d4fe341c4afbe76e701524da2ea37589f91c82
OKAN CANDENİZ <candenizokan@gmail.com>
2023-09-24 10:51:54 +0300

    _categoryRepository,_userManager

--- Blog.Web/Areas/Member/Views/Shared/Components/FollowedCategory/FollowedCategoryViewComponent.cs
+++ Blog.Web/Areas/Member/Views/Shared/Components/FollowedCategory/FollowedCategoryViewComponent.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Dal.Repositories.Interfaces.Concrete;
+using Blog.Model.Models.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Member.Views.Shared.Components.FollowedCategory
 {
     public class FollowedCategoryViewComponent:ViewComponent
     {
         //kullanıcının takip ettiği kategorileri sunmak
+
+        //kategori repo içinde ihtiyacım olan metodu oluşturacğım.
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly UserManager<AppUser> _userManager;
+
+        public FollowedCategoryViewComponent(ICategoryRepository categoryRepository,UserManager<AppUser> userManager)
+        {
+            _categoryRepository = categoryRepository;
+            _userManager = userManager;
+        }
     }
 }
